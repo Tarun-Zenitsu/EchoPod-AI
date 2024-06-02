4b65d4d800f2853aebfb96984c7dd54068a087ee
Tarunakanta kumbhar <btkkumbhar1998@gmail.com>
2024-06-02 15:38:02 +0530

    setup completed

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="mt-9 flex flex-col gap-9">
+      <section className="flex flex-col gap-5">
+        <h1 className="text-20 font-bold text-white-1">Trending podcast</h1>
+        <Button className="text-white-1 bg-orange-1">hi there</Button>
+      </section>
+    </div>
+  );
+}
